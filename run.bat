8d5c66cd6c4643c52800d837f98f5b080377e645
yanshiqwq <yanshiqwq@outlook.com>
2024-02-20 00:40:33 +0800

    2024-02-20_00-40-32

--- run.bat
+++ run.bat
@@ -3,6 +3,7 @@ python e5_usage_sync.py --input %cd%\README.template.md --output %cd%\README.md
 ::python upload.py
 set dtTmp=%date:~0,4%-%date:~5,2%-%date:~8,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
 set dt=%dtTmp: =0%
-git add README.md
+git add *
 git commit -m %dt%
+git push
 pause
